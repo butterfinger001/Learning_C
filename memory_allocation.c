05eb889075fba8861a62f5742ec0e291db56da27
butterfinger001 <rs0587u@gre.ac.uk>
2024-10-21 15:44:23 +0100

    mod: I worked on lab 6 pointers and addressing.

---
+++ memory_allocation.c
@@ -0,0 +1,9 @@
+int main ()
+{
+	int n = 11;
+
+	int *ptrToN = n;
+	printf("n's address: %d and %d ptrToN value \n", &n, ptrTon);
+	printf("n's value: %d and ptrToN points to value %d \n", n, *ptrToN);
+	return 0;
+}
